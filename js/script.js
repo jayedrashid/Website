7dc246d6970d7635c04374463306c936a3678752
Jayed Rashid <user@users-MacBook-Air.local>
2022-08-26 21:50:09 +0600

    added dark mode

--- js/script.js
+++ js/script.js
@@ -67,3 +67,18 @@ scrollBtn.addEventListener('click', () => {
 document.addEventListener('scroll', (e) => {
     refreshButtonVisibility();
 });
+
+
+
+// Dark Theme Toggle
+var icon = document.getElementById('icon');
+
+icon.onclick = function() {
+    document.body.classList.toggle('dark-theme');
+    if (document.body.classList.contains('dark-theme')) {
+        icon.src = 'img/sun.png';
+    } else {
+        icon.src = 'img/moon.png';
+    }
+};
+
